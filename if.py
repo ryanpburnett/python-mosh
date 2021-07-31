4da15c343e62a6ed9b32fb23e681f9a167670253
Ryan <ryanpburnett@yahoo.com>
2021-07-31 16:05:51 -0500

    timestamp 1:12:41

---
+++ if.py
@@ -0,0 +1,11 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("It's hot, have a glass of water")
+elif is_cold:
+    print("It's cold, grab a sweater")
+else:
+    print("great weather today")
+
+print("enjoy your day")
---
+++ logicalOperators.py
@@ -0,0 +1,11 @@
+likes_walks = True
+is_barking = False
+
+if likes_walks and is_barking:
+    print("must be a dog")
+
+
+# 'or' can also be used as a logical operator
+
+if likes_walks and not is_barking:
+    print('ready for a walk')
